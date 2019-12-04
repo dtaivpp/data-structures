c1e2164a5dfa3d03d4037625f064ff06bf4b8425
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2019-12-04 08:46:01 -0500

    b-tree start

---
+++ b-tree/b_tree.py
@@ -0,0 +1,10 @@
+
+
+class BTree:
+    def __init__(self, root=None):
+        self.root = root
+    
+    def insert_node(self):
+        pass
+
+ 
---
+++ b-tree/node.py
@@ -0,0 +1,7 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+    
